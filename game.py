6f8376c5986529d6c919705b5ad2b2e1ded6975e
Ruhan1147 <140265957+Ruhan1147@users.noreply.github.com>
2025-06-27 17:31:00 +0100

    Update game.py

--- game.py
+++ game.py
@@ -1,112 +1,122 @@
 import random
 
-class board:
+class Board:
     def __init__(self, dim_size, num_bombs):
-
         self.dim_size = dim_size
         self.num_bombs = num_bombs 
-
-        self.board = self.make_new_board
-
-
+        self.board = self.make_new_board()
+        self.assign_values_to_board()
         self.dug = set()
     
     def make_new_board(self):
         board = [[None for _ in range(self.dim_size)] for _ in range(self.dim_size)] 
-        
         bombs_planted = 0
         while bombs_planted < self.num_bombs:
             loc = random.randint(0, self.dim_size**2 - 1)
             row = loc // self.dim_size
             col = loc % self.dim_size
-            
             if board[row][col] == '*':
                 continue
             board[row][col] = '*'
             bombs_planted += 1
-        return  board
+        return board
     
     def assign_values_to_board(self):
         for r in range(self.dim_size):
             for c in range(self.dim_size):
                 if self.board[r][c] == '*':
-                     continue
-                self.board[r][c] = self.get_num_neighbouring_bombs(r,c)
+                    continue
+                self.board[r][c] = self.get_num_neighbouring_bombs(r, c)
     
-    def get_num_neighbouring(self, row, col):
-
-
+    def get_num_neighbouring_bombs(self, row, col):
         num_neighbouring_bombs = 0
-        for r in range(max(0,row-1), min(self.dim_size-1,row + 1)+1):
-            for c in range(max(0, col-1), min(self.dim_size, col+1)+1):
+        for r in range(max(0, row-1), min(self.dim_size-1, row+1)+1):
+            for c in range(max(0, col-1), min(self.dim_size-1, col+1)+1):
                 if r == row and c == col:
-                    #org location - dont check
                     continue
                 if self.board[r][c] == '*':
                     num_neighbouring_bombs += 1
+        return num_neighbouring_bombs
     
     def dig(self, row, col):
-
-        #dig at loc
-        #return true if successful, else false if bomb
-
-        #secnarios:
-        #dig = bomb = game over
-        #dig = no neighbouring bombs = recursively dig neighboring
-        
-        self.dug.add(row,col)
-
+        self.dug.add((row, col))
         if self.board[row][col] == '*':
             return False
         elif self.board[row][col] > 0:
             return True
         for r in range(max(0, row-1), min(self.dim_size-1, row+1)+1):
-            for c in range(max(0, row-1), min(self.dim_size-1, row+1)+1):
+            for c in range(max(0, col-1), min(self.dim_size-1, col+1)+1):
                 if (r, c) in self.dug:
                     continue
                 self.dig(r, c)
         return True
 
     def __str__(self):
-
-
         visible_board = [[None for _ in range(self.dim_size)] for _ in range(self.dim_size)]
         for row in range(self.dim_size):
             for col in range(self.dim_size):
-                if (row,col) in self.dug:
+                if (row, col) in self.dug:
                     visible_board[row][col] = str(self.board[row][col])
                 else:
-                    visible_board[row][col] = ' ' 
+                    visible_board[row][col] = ' '
+        string_rep = ''
+        widths = []
+        for idx in range(self.dim_size):
+            columns = map(lambda x: x[idx], visible_board)
+            widths.append(
+                len(
+                    max(columns, key=len)
+                )
+            )
 
+        indices = [i for i in range(self.dim_size)]
+        indices_row = '   '
+        cells = []
+        for idx, col in enumerate(indices):
+            format = '%-' + str(widths[idx]) + "s"
+            cells.append(format % (col))
+        indices_row += '  '.join(cells)
+        indices_row += '   \n'
 
-        
+        for i in range(len(visible_board)):
+            row = visible_board[i]
+            string_rep += f'{i} |'
+            cells = []
+            for idx, col in enumerate(row):
+                format = '%-' + str(widths[idx]) + "s"
+                cells.append(format % (col))
+            string_rep += ' |'.join(cells)
+            string_rep += ' |\n'
+
+        str_len = int(len(string_rep) / self.dim_size)
+        string_rep = indices_row + '-'*str_len + '\n' + string_rep + '-'*str_len
+
+        return string_rep
+
 def play(dim_size=10, num_bombs=19):
-    #1. create board and plant bombs.
-    board =  board(dim_size, num_bombs)
-    #2. show board and ask user where they want to dig
-    #3a. if bomb, show game over
-    #3b. if not bomb, recursively dig until each square near bomb
-    #4. repeat 2 and 3a/b until no more spaces left to dig
+    board = Board(dim_size, num_bombs)
     safe = True
     while len(board.dug) < board.dim_size ** 2 - num_bombs:
         print(board)
-        user_input = input("Where would you like to dig? input a row,col")
-        row, col = int(user_input[0]), int(user_input(-1))
-        if row < 0 or row >= board.dim_size or col < 0 or col  >= dim_size:
-            print("Invalid location, try agaim")
+        user_input = input("Where would you like to dig? input a row,col: ")
+        try:
+            row, col = map(int, user_input.split(','))
+        except ValueError:
+            print("Invalid input. Try again.")
             continue
-
-        safe = board.dig(row,col)
+        if row < 0 or row >= board.dim_size or col < 0 or col >= board.dim_size:
+            print("Invalid location, try again.")
+            continue
+        safe = board.dig(row, col)
         if not safe:
             break
     
     if safe:
-        print("u win")
-
+        print("Congratulations, you win!")
     else:
-        print("u lose")
-        board.dug = [(r,c) for r in range(board.dim_size) for c in range(board.dim_size)]
+        print("Sorry, you hit a bomb! Game over.")
+        board.dug = {(r, c) for r in range(board.dim_size) for c in range(board.dim_size)}
         print(board)
 
-if __name__ == '__main_':
+if __name__ == '__main__':
     play()
